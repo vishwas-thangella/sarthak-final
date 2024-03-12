2fcfb98ed0b9da7488aed54d86b3fd854e4dd77b
Vishwas <vishwas56883669@gmail.com>
2024-03-12 23:52:23 +0530

    final commit

--- server/settings.py
+++ server/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
